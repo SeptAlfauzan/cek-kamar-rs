b61d28d9818a212c97855a0a820986e7c42638fd
SeptAlfauzan <alfauzansepta@gmail.com>
2021-09-19 10:04:50 +0700

    fix the port

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const Data = require('./Data.js');
 
 app.get('/', (req, res)=>{
     res.send('Welcome');
 });
+
 app.get('/tes', (req, res)=>{
     Data.getHospitalsName().then(result => {
         console.log(result);
         res.send(result);
     }).catch(reject => console.log(reject));
 });
+
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
 });
