c0ad66cf73d1232cc82bd6ea328dbc54c4529fdb
SeptAlfauzan <alfauzansepta@gmail.com>
2021-09-30 21:17:01 +0700

    change content type to json

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
 const port = process.env.PORT || 3000;
 
 const Data = require('./Data.js');
 
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res)=>{
-    res.send('Welcome, for documentation check this <a href="https://github.com/SeptAlfauzan/cek-kamar-rs">link</a>');
+    res.render('home');
+    // res.send('Welcome, for documentation check this <a href="https://github.com/SeptAlfauzan/cek-kamar-rs">link</a>');
+});
+app.get('/learn', (req, res)=>{
+    res.render('tailwind');
+    // res.send('Welcome, for documentation check this <a href="https://github.com/SeptAlfauzan/cek-kamar-rs">link</a>');
 });
 
 app.get('/provinsi', (req, res)=>{
    Data.getAllProvData().then(result => {
        console.log(result);
-       res.send(result);
+       res.setHeader('Content-Type', 'application/json');
+       res.send(JSON.stringify(result));
    }).catch(reject => console.log(reject));
 });
 app.get('/kabupaten', (req, res)=>{
@@ -19,7 +29,8 @@ app.get('/kabupaten', (req, res)=>{
     if (!prov) res.sendStatus(404);
     Data.getKabKotaData(prov).then(result => {
         console.log(result);
-        res.send(result);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(result));
     }).catch(reject => console.log(reject));
 });
 
@@ -28,7 +39,8 @@ app.get('/rumahsakit', (req, res)=>{
     const city = req.query.city;
     Data.getHospitalsName(prov, city).then(result => {
         console.log(result);
-        res.send(result);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(result));
     }).catch(reject => console.log(reject));
 });
 
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontSize: {
+        giant: '10rem',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
